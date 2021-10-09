d55c5dc5e16b65dd41e8892971249c600815cdf6
r0xANDt0l <41841903+r0xANDt0l@users.noreply.github.com>
2021-10-09 12:11:07 +0200

    First projects

---
+++ Proyecto1/Archivo1.py
@@ -0,0 +1,24 @@
+
+from typing import Collection
+
+
+x = int(input("Introduzca el primer número: "))
+y = int(input("Introduzca el segundo nùmero: "))
+
+print("el resultado es: ", x + y)
+
+# if (x + y) % 2 == 0:
+#    print("Es par")
+# else:
+#    print("Es impar")
+
+# while i <= (x+13):
+#     print(i)
+#     i += 1
+
+# range(x+y)
+
+# for i in range(x + y+1):
+#     print(i)
+
+print(x[0])
---
+++ Proyecto1/calc.py
@@ -0,0 +1,43 @@
+# nombre = "Rodrigo"
+
+# premi = nombre[0:5]
+# segu = nombre[5:]
+
+# print(premi)
+# print(segu)
+
+ac = float(input("Introduzca un valor inicial: "))
+
+while True:
+    print(ac)
+    x = input(ac)
+
+    ope = x[0]
+  
+
+    if ope == 'cl':
+        ac = 0
+        continue
+    elif ope == 'exit':
+        break
+
+    num = float(x[1:])
+    
+    if ope == '+':
+        ac+=num
+    if ope == '-':
+        ac-=num   
+    if ope == '*':
+        ac*=num  
+    if ope == ':':
+        ac/=num
+    if ope == '^':
+        ac **= num
+    if ope == 'r':
+        ac **= 1/num
+        
+    
+        
+
+
+
