3c292273a81230805b690b493729c2767b8181e6
r0xANDt0l <rodrigosgdlcc@gmail.com>
2021-10-16 11:02:36 +0200

    Add time

--- Proyecto1/rndTest.py
+++ Proyecto1/rndTest.py
@@ -1,16 +1,20 @@
 import random
+import time
 
 minN = int(input("Dime el primer numero: "))
 maxN = int(input("Dime el segundo numero: "))
-atts = 0
+atts = 1
 
 N = int(input("Dime el numero que intentas buscar: "))
+TimeStr = time.time()
 
 while N >= minN and N <= maxN:
     rnd = random.randint(minN, maxN)
     if N == rnd:
-        print("Encontrado, me ha llevado esta cantidad de intentos: ")
-        print(atts)
+        TimeEnd = time.time()
+        TimeTok = TimeEnd - TimeStr
+        print("Encontrado, me ha llevado ", atts,
+              " intentos y me ha llevado ", TimeTok, "seg")
         break
     else:
         atts += 1
