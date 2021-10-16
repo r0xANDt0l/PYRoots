7110de1a71ea6ce7fc44d19294569f7164aee535
r0xANDt0l <rodrigosgdlcc@gmail.com>
2021-10-16 10:30:35 +0200

    Random library test

---
+++ Proyecto1/rndTest.py
@@ -0,0 +1,16 @@
+import random
+
+minN = int(input("Dime el primer numero: "))
+maxN = int(input("Dime el segundo numero: "))
+atts = 0
+
+N = int(input("Dime el numero que intentas buscar: "))
+
+while N >= minN and N <= maxN:
+    rnd = random.randint(minN, maxN)
+    if N == rnd:
+        print("Encontrado, me ha llevado esta cantidad de intentos: ")
+        print(atts)
+        break
+    else:
+        atts += 1
