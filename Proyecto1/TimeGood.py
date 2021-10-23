6e2f3fde254f60ce8d6a2b0c8fa6d6d4c5b16a81
r0xANDt0l <rodrigosgdlcc@gmail.com>
2021-10-23 11:05:00 +0200

    years

--- Proyecto1/TimeGood.py
+++ Proyecto1/TimeGood.py
@@ -5,13 +5,15 @@ hours = 0
 days = 0
 years = 0
 
-t = 3599
+t = time.time()
 
 while True:
-    print(years, " : ", days, " : ", hours, " : ", mins, " : ", secs) # Imprimir los numeros
+    print(int(years), "y ", int(days), "d ", int(hours), "h ", int(mins), "m ", int(secs), "s") # Imprimir los numeros
 
-    secs = int(t%60) # Obtener resto de t / 60
-    mins = int((t//60)%60) # Dividir t / 60 y asegurarse de que solo sea entero
-    hours = int(t//3600) # obtener horas dividiendo t entre 3600 (60 mins)
+    secs = t%60 # Obtener resto de t / 60
+    mins = (t//60)%60 # Dividir t / 60 y asegurarse de que solo sea entero
+    hours = (t//3600)%24 # obtener horas dividiendo t entre 3600 (60 mins)
+    days = (t//86400) %365
+    years = t//31536000
     time.sleep(1) #
     t +=1
