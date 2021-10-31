87d2e1f7d574d13ec8965faf3bd6bef85043df51
r0xANDt0l <rodrigosgdlcc@gmail.com>
2021-10-31 16:04:55 +0100

    Added sides selector (still buggy)

--- Proyecto1/RollTheDice.py
+++ Proyecto1/RollTheDice.py
@@ -1,8 +1,19 @@
 import keyboard #Importar la libreria de cosas de teclado
 import random #Importar la libreria de cosas random
+import time
 
-input('Press enter to roll the dice') #Pedir pulsar el enter para empezar
+sides = 1
+sidesSelect = input("Amount of sides the dice has. If empty, 6: ")
+is_non_empty= bool(sidesSelect)
+if is_non_empty == False:
+    sides = 6
+else:
+    sides = sidesSelect
+
+time.sleep(0.5)
+
 while True: #Un loop
-    nmb = random.randint(1,6) #Obtener numero al azar
+    nmb = random.randint(1,sides) #Obtener numero al azar
     print("The dice rolled ", nmb) #Decir el numero que ha tocado
     input('Press enter to roll the dice again') #Decir si quiere tirar de nuevo
+    time.sleep(random.uniform(0.2,0.8))
