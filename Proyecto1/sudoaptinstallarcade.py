341d9644a78db72a0a2b539b60bce36a19636e6f
r0xANDt0l <41841903+r0xANDt0l@users.noreply.github.com>
2021-11-06 12:05:05 +0100

    Lamp post works

--- Proyecto1/sudoaptinstallarcade.py
+++ Proyecto1/sudoaptinstallarcade.py
@@ -5,7 +5,7 @@ width = 800
 height = 600
 
 def StopSign(X,Y,SizeOfCircle):
-    arcade.draw_rectangle_filled(random.randint(X*0.9,X*1.1), Y/2, SizeOfCircle/10, SizeOfCircle*3, color.BRONZE)
+    arcade.draw_rectangle_filled(random.uniform(X*0.90,X*1.19), Y/2, SizeOfCircle/10, SizeOfCircle*3, color.BRONZE)
     arcade.draw_circle_filled(X, Y ,SizeOfCircle, color.RED_DEVIL)
     arcade.draw_rectangle_filled(X, Y, SizeOfCircle*1.5, SizeOfCircle/2.5, color.BABY_POWDER)
 
