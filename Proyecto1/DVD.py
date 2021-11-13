bd1bf42cea456c5a83ea370d4fd9654494cb50d2
r0xANDt0l <41841903+r0xANDt0l@users.noreply.github.com>
2021-11-13 11:15:56 +0100

    b o u n c e + color change

--- Proyecto1/DVD.py
+++ Proyecto1/DVD.py
@@ -3,27 +3,36 @@ from arcade import color
 from random import randint
 
 def draw(deltaTime):
-    global posX
+    global posX, posY, movX, movY, size, colour
 
     arcade.start_render()
     arcade.set_background_color(color.AIR_FORCE_BLUE)
 
-    arcade.draw_rectangle_filled(posX,posY,size,size,color.RED)
+    arcade.draw_rectangle_filled(posX,posY,size,size,(colour,colour,colour))
 
-    posX += 100 * deltaTime
+    posX += movX
+    posY += movY
 
-    arcade.finish_render
+    if posX >= windW-size/2 or posX <= 0+size/2:
+        movX *= -1
+        colour = randint(0,255)
+    if posY >= windH-size/2 or posY <= 0+size/2:
+        movY *= -1 
+        colour = randint(0,255)
+    
 
 windW = 800
 windH = 600
 
-posX = 0
+posX = windW/2
 posY = windH/2
-movX = 5
-movY = 5
+movX = 2
+movY = 2
 size = 50
+colour = 255
 
 arcade.open_window(windW,windH, "DVD")
 
-arcade.schedule(draw,1/144)
+arcade.schedule(draw,1/60)
+
 arcade.run()
