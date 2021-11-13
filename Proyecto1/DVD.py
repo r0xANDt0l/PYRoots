8d1a292aa1e79ded6bf19e55bfaa22ca2afb0c3c
r0xANDt0l <41841903+r0xANDt0l@users.noreply.github.com>
2021-11-13 11:26:11 +0100

    converted important variables to arrays

--- Proyecto1/DVD.py
+++ Proyecto1/DVD.py
@@ -8,17 +8,17 @@ def draw(deltaTime):
     arcade.start_render()
     arcade.set_background_color(color.AIR_FORCE_BLUE)
 
-    arcade.draw_rectangle_filled(posX,posY,size,size,(colour1,colour2,colour3))
+    arcade.draw_circle_filled(posX,posY,size,(colour1,colour2,colour3))
 
     posX += movX
     posY += movY
 
-    if posX >= windW-size/2 or posX <= size/2:
+    if posX >= windW-size/2 or posX <= size:
         movX *= -1
         colour1 = randint(0,255)
         colour2 = randint(0,255)
         colour2 = randint(0,255)
-    if posY >= windH-size/2 or posY <= size/2:
+    if posY >= windH-size/2 or posY <= size:
         movY *= -1 
         colour1 = randint(0,255)
         colour2 = randint(0,255)
@@ -26,14 +26,14 @@ def draw(deltaTime):
 windW = 800
 windH = 600
 
-posX = windW/2
-posY = windH/2
-movX = 5
-movY = 5
-size = 100
-colour1 = 255
-colour2 = 255
-colour3 = 255
+posX = [windW/2]
+posY = [windH/2]
+movX = [2]
+movY = [2]
+size = 25
+colour1 = [255]
+colour2 = [255]
+colour3 = [255]
 
 arcade.open_window(windW,windH, "DVD")
 
