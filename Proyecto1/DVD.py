3e92c3c5f066dd936b95432ea515154bbfff280a
r0xANDt0l <41841903+r0xANDt0l@users.noreply.github.com>
2021-11-13 10:51:12 +0100

    made it move depending with FPS

--- Proyecto1/DVD.py
+++ Proyecto1/DVD.py
@@ -3,17 +3,21 @@ from arcade import color
 from random import randint
 
 def draw(deltaTime):
+    global posX
+
     arcade.start_render()
     arcade.set_background_color(color.AIR_FORCE_BLUE)
 
-    arcade.draw_rectangle_filled(randint(0,windW),posY,size,size,color.RED)
+    arcade.draw_rectangle_filled(posX,posY,size,size,color.RED)
+
+    posX += 100 * deltaTime
 
     arcade.finish_render
 
 windW = 800
 windH = 600
 
-posX = windW/2
+posX = 0
 posY = windH/2
 movX = 5
 movY = 5
@@ -21,5 +25,5 @@ size = 50
 
 arcade.open_window(windW,windH, "DVD")
 
-arcade.schedule()
+arcade.schedule(draw,1/144)
 arcade.run()
