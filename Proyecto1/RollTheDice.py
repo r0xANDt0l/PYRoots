da14d326ce8c80ea7f091f5084de4dc070c19501
r0xANDt0l <rodrigosgdlcc@gmail.com>
2021-11-14 01:16:37 +0100

    now it doesn't crash

--- Proyecto1/RollTheDice.py
+++ Proyecto1/RollTheDice.py
@@ -2,18 +2,12 @@ import keyboard #Importar la libreria de cosas de teclado
 import random #Importar la libreria de cosas random
 import time
 
-sides = 1
-sidesSelect = input("Amount of sides the dice has. If empty, 6: ")
-is_non_empty= bool(sidesSelect)
-if is_non_empty is False:
-    sides = 6
-else:
-    sides = sidesSelect
+sidesSelect = int(input("Amount of sides the dice has. If empty, 6: ") or "6")
 
 time.sleep(0.5)
 
 while True: #Un loop
-    nmb = random.randint(1,sides) #Obtener numero al azar
+    nmb = random.randint(1,sidesSelect) #Obtener numero al azar
     print("The dice rolled ", nmb) #Decir el numero que ha tocado
     input('Press enter to roll the dice again') #Decir si quiere tirar de nuevo
     time.sleep(random.uniform(0.2,0.8))
