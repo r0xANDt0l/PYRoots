80c59b3a1bbf43bc302cddab57dd281730db30ff
r0xANDt0l <rodrigosgdlcc@gmail.com>
2021-11-20 11:33:48 +0100

    learnt to use vectors, and now bals crash

--- Proyecto1/DVD.py
+++ Proyecto1/DVD.py
@@ -1,5 +1,6 @@
 import numbers
 import arcade
+import math
 from arcade import color
 from random import randint
 
@@ -7,14 +8,34 @@ from random import randint
 def RNDColor():
     return(randint(0,255) ,randint(0,255) ,randint(0,255) )
 
+def CheckCollision(i : int, j : int):
+    if i == j:  
+        return
+        
+    x = posX[j]-posX[i]
+    y = posY[j]-posY[i]
+
+    mod = math.sqrt( x*x + y*y )
+    if mod <= radius*2:
+        x /= mod
+        y /= mod
+        x *=speed
+        y *= speed
+        movX[i] = -x
+        movY[i] = -y
+        movX[j] = x
+        movY[j] = y
+
+
+
 def movement( i : int):
     posX[i] += movX[i]
     posY[i] += movY[i]
 
-    if posX[i] >= windW-size or posX[i] <= size:
+    if posX[i] >= windW-radius or posX[i] <= radius:
         movX[i] *= -1
         colour[i] = RNDColor()
-    if posY[i] >= windH-size or posY[i] <= size:
+    if posY[i] >= windH-radius or posY[i] <= radius:
         movY[i] *= -1 
         colour[i] = RNDColor()
 
@@ -23,22 +44,44 @@ def draw(deltaTime):
     arcade.start_render()
 
     for i in range(numbS):
-        arcade.draw_circle_filled(posX[i],posY[i],size,colour[i])
+        arcade.draw_circle_filled(posX[i],posY[i],radius,colour[i])
 
         movement(i)
+
+        for j in range(numbS):
+            if i == j:
+                continue
+            CheckCollision(i,j)
 
        
 windW = 800
 windH = 600
 
 
-size = 25
+radius = 25
 numbS = randint(1,10)
-posX = [randint(size,windW-size) for i in range(numbS)]
-posY = [randint(size,windH-size) for i in range(numbS)]
+speed = randint(1,10)
+posX = [randint(radius,windW-radius) for i in range(numbS)]
+posY = [randint(radius,windH-radius) for i in range(numbS)]
 
-movX = [randint(-10,10) for i in range(numbS)]
-movY = [randint(-10,10) for i in range(numbS)]
+movX = [randint(-1000,1000) for i in range(numbS)]
+movY = [randint(-1000,1000) for i in range(numbS)]
+
+for i in range(numbS):
+    x = movX[i]
+    y = movY[i]
+    mod = math.sqrt(x*x+y*y)
+
+    x/= mod
+    y/= mod
+
+    x *= speed
+    y *= speed
+
+    movX[i] = x
+    movY[i] = y
+
+
 colour = [RNDColor() for i in range(numbS)]
 
 arcade.open_window(windW,windH, "DVD")
---
+++ Proyecto1/normalizeVectors.py
@@ -0,0 +1,11 @@
+import math
+
+x = 10
+y = 15
+
+module = math.sqrt( x*x + y*y )
+
+x /= module
+y /= module
+
+print(math.sqrt( x*x + y*y ))
