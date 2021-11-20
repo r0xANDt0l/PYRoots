37f4fae4a48ab395633edb50160438e7cc130bc6
r0xANDt0l <rodrigosgdlcc@gmail.com>
2021-11-20 10:26:16 +0100

    moved movement things

--- Proyecto1/DVD.py
+++ Proyecto1/DVD.py
@@ -7,32 +7,35 @@ from random import randint
 def RNDColor():
     return(randint(0,255) ,randint(0,255) ,randint(0,255) )
 
+def movement( i : int):
+    posX[i] += movX[i]
+    posY[i] += movY[i]
+
+    if posX[i] >= windW-size or posX[i] <= size:
+        movX[i] *= -1
+        colour[i] = RNDColor()
+    if posY[i] >= windH-size or posY[i] <= size:
+        movY[i] *= -1 
+        colour[i] = RNDColor()
+
 def draw(deltaTime):
     
     arcade.start_render()
 
     for i in range(numbS):
-        arcade.set_background_color(color.AIR_FORCE_BLUE)
-
         arcade.draw_circle_filled(posX[i],posY[i],size,colour[i])
 
-        posX[i] += movX[i]
-        posY[i] += movY[i]
+        movement(i)
 
-        if posX[i] >= windW-size or posX[i] <= size:
-            movX[i] *= -1
-            colour[i] = RNDColor()
-        if posY[i] >= windH-size or posY[i] <= size:
-            movY[i] *= -1 
-            colour[i] = RNDColor()
+       
 windW = 800
 windH = 600
 
 
 size = 25
-numbS = randint(1,25)
+numbS = randint(1,10)
 posX = [randint(size,windW-size) for i in range(numbS)]
-posY = [randint(size,windW-size) for i in range(numbS)]
+posY = [randint(size,windH-size) for i in range(numbS)]
 
 movX = [randint(-10,10) for i in range(numbS)]
 movY = [randint(-10,10) for i in range(numbS)]
